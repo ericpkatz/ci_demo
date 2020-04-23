f41a79a8c3b7f1cfc45cfc7f325fff4362385457
ericpkatz <ericpkatz@gmail.com>
2020-04-23 19:20:36 -0400

    add sequelize pg

--- spec.js
+++ spec.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
+const { STRING } = Sequelize;
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db_test');
+
+const User = conn.define('user', {
+  name: STRING 
+});
 
 const _app = require('supertest')(app);
 
-app.get('/api/users', (req, res, next)=> res.send([ { name: 'Lucy' }]));
+app.get('/api/users', async(req, res, next)=> res.send(await User.findAll()));
 
 const { expect } = require('chai');
 
@@ -14,10 +21,17 @@ describe('the truth', ()=> {
 });
 
 describe('GET /api/users', ()=> {
+  beforeEach(async()=> {
+    await conn.sync({ force: true });
+    await Promise.all([
+      User.create({ name: 'Lucy' }),
+      User.create({ name: 'Moe' }),
+    ]);
+  });
   it('returns the users', async()=> {
 
     const response = await _app.get('/api/users');
     expect(response.status).to.equal(200);
-    expect(response.body.length).to.equal(1);
+    expect(response.body.length).to.equal(2);
   });
 });
