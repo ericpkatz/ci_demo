2c35c2a16804a3b840845763e0ab10ae87301969
ericpkatz <ericpkatz@gmail.com>
2020-04-23 19:14:51 -0400

    added express app

--- spec.js
+++ spec.js
@@ -1,6 +1,23 @@
+const express = require('express');
+const app = express();
+
+const _app = require('supertest')(app);
+
+app.get('/api/users', (req, res, next)=> res.send([ { name: 'Lucy' }]));
+
 const { expect } = require('chai');
+
 describe('the truth', ()=> {
   it('true to equal true', ()=> {
     expect(true).to.equal(true);
+  });
+});
+
+describe('GET /api/users', ()=> {
+  it('returns the users', async()=> {
+
+    const response = await _app.get('/api/users');
+    expect(response.status).to.equal(200);
+    expect(response.body.length).to.equal(1);
   });
 });
